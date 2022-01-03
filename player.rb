3c0fc66870d64fd1440b0b5ac7886417dda951b4
stew anoya <stewartanoya@gmail.com>
2022-01-03 19:01:20 +0000

    Create player object

---
+++ player.rb
@@ -0,0 +1,20 @@
+class Players
+
+  attr_accessor :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+
+  def player1_lives 
+    3
+  end
+
+  def player2_lives 
+    3
+  end
+
+
+end
