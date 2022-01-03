4d931c1c3230c854ae58e73c2c4c3f81bf121123
stew anoya <stewartanoya@gmail.com>
2022-01-03 19:01:31 +0000

    Add require and test code for player object

---
+++ adding_game.rb
@@ -0,0 +1,20 @@
+#------classes
+# Player
+## Role - will answer questions in prompt
+## Storing - Player name, score, current player
+## Methods - game over, add/remove life
+
+
+# Question
+## will pick 2 random numbers and display those as a question
+## Storing - string for question, maybe even a response
+## Methods - new question, is correct?
+## will contain i/o
+
+
+# Turn
+## Will display a new turn 
+## storing - a variable to know what turn round it is, who is going next
+## Methods - Will check if game over
+
+
---
+++ main.rb
@@ -0,0 +1,5 @@
+require './player'
+
+new_players = Players.new("stew", "kelsey")
+puts new_players.player1
+puts new_players.player2
